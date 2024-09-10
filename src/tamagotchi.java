49622fc24d6bc15c528e9d3b2c3a2498bd45fb6a
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2024-09-10 11:04:02 +0200

    alla klasser osp

---
+++ src/tamagotchi.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class tamagotchi{
+
+    private int Hunger;
+    private int Boredom;
+    private int Words;
+    private boolean IsAlive;
+    private Random Generator;
+    public String Name;
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Tick()
+    {
+        
+    }
+
+    public void PrintStats()
+    {
+
+    }
+
+    public boolean GetAlive()
+    {
+        return true;
+    }
+
+    private void Reduceboredom()
+    {
+
+    }
+}
