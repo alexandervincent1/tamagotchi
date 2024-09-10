cbcaf0dadd7523d94c435783298136287fc7645d
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2024-09-10 10:38:32 +0200

    tamagotchi

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+    Scanner tangentbord = new Scanner(System.in);
+
+    System.out.println("Välj namn på din tamagotchi");
+    String Name = tangentbord.nextLine();
+    System.out.println("\n1.Lär ett nytt ord.\n2.Hälsa på din tamagotchi.\n3.Mata din tamagotchi.\n4.Gör ingeting");
+    int val = tangentbord.nextInt();
+    if (val==1) {
+        
+    }
+
+    if (val==2) {
+        
+    }
+
+    if (val==3) {
+        
+    }
+
+    if (val==4) {
+        
+    }
+    }
+}
